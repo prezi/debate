442d4b4535d56f4e46a85b929698ef0a04fa6379
Elise Huard <elise@jabberwocky.eu>
2014-08-27 10:00:23 +0200

    handle /info request by sockjs (not working)

--- src/Main.hs
+++ src/Main.hs
@@ -1,12 +1,38 @@
+{-# LANGUAGE TemplateHaskell #-}
 import           Control.Monad      (forever)
-import qualified Data.Text          as T
 import qualified Network.WebSockets as WS
 
+import Network.HTTP.Types (status200)
+import qualified Network.Wai as Wai
+import qualified Network.Wai.Handler.Warp as Warp
+import qualified Network.Wai.Handler.WebSockets as WaiWS
+import qualified Network.Wai.Application.Static as Static
+import Blaze.ByteString.Builder (copyByteString)
 
-main = WS.runServer "0.0.0.0" 8888 echo
+import qualified Data.ByteString.Char8 as B
+import qualified Data.Text          as T
+import qualified Data.CaseInsensitive as CI ( mk )
+
+import Data.Aeson
 
 
-echo pending = do
+-- info query should return something sensible, and then move on to websocket
+main = do
+    Warp.runSettings Warp.defaultSettings
+      { Warp.settingsPort = 8888 } $ WaiWS.websocketsOr WS.defaultConnectionOptions application httpApplication
+
+httpApplication :: Wai.Application
+httpApplication req respond = respond $ Wai.responseBuilder status200 [(CI.mk (B.pack "Content-Type"), B.pack "application/json")] $ copyByteString (B.pack "")
+
+{-
+httpApplication :: Application
+httpApplication req = responseBuilder status200 [("Content-Type", "application/json")] $ encode []
+-}
+
+application pending = do
     connection <- WS.acceptRequest pending
+    echo connection
+
+echo connection = forever $ do
     msg <- (WS.receiveData connection) :: IO T.Text
     WS.sendTextData connection $ msg
--- test/config.js
+++ test/config.js
@@ -3,5 +3,10 @@ var config = {};
 
 config.websocketServer = 'node'
 config.websocketArgs = [path.resolve(__dirname, '../websocket.js')]
+/*
+config.websocketServer = path.resolve(__dirname, '..', 'dist/build/debate/debate');
+config.websocketArgs = [];
+*/
+console.log(config);
 
 module.exports = config
