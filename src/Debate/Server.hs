861e42d190edbc3d57e30dd38bc9f2b49eb4e8d8
Elise Huard <elise@jabberwocky.eu>
2014-08-28 14:42:58 +0200

    first step of refactoring - everything in server

---
+++ src/Debate/Server.hs
@@ -0,0 +1,94 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Debate.Server 
+( runServer
+, Config(..)
+, httpApplication
+, wsApplication
+, receiveData
+, sendTextData
+, SockJSMessage(..)
+)
+where
+
+import           Control.Monad.Trans (liftIO)
+import           Control.Concurrent (forkIO, threadDelay)
+import           System.Random (randomRIO)
+import           Data.Maybe (fromMaybe)
+
+import qualified Network.WebSockets as WS
+import qualified Network.HTTP.Types          as H
+import qualified Network.HTTP.Types.Header   as H
+import qualified Network.Wai as Wai
+import qualified Network.Wai.Handler.Warp as Warp
+import qualified Network.Wai.Handler.WebSockets as WaiWS
+import Blaze.ByteString.Builder (fromLazyByteString)
+
+import qualified Data.Text          as T
+import qualified Data.ByteString    as B
+
+import Data.Aeson
+
+-- TODO add prefix to config
+data Config = Config { port :: Int }
+
+data SockJSMessage = SockJSMessage [T.Text]
+
+runServer configuration application = do
+    let settings = Warp.setPort (port configuration) Warp.defaultSettings
+    Warp.runSettings settings $ WaiWS.websocketsOr WS.defaultConnectionOptions (wsApplication application) (httpApplication application)
+
+-- TODO: add routing for '/info', '/greeting', and all xhr polling
+-- the websocket or xhr-polling transport should go over 
+-- /prefix/server id/session id unique to session
+-- add '/websocket' for websocket transport
+-- add '/xhr' for xhr-polling
+-- todo routing on prefix too (threading)
+httpApplication :: (WS.Connection -> IO ()) -> Wai.Application
+httpApplication application req respond = do ent <- liftIO $ randomRIO ((0, 4294967295) :: (Int, Int))
+                                             respond $ Wai.responseBuilder H.status200 
+                                                    (concat [headerJSON, headerNotCached, headerCORS "*" req])
+                                                    $ fromLazyByteString $ encode         [ "websocket"     .= True
+                                                                                        , "cookie_needed" .= False
+                                                                                        , "origins"       .= ["*:*" :: T.Text]
+                                                                                        , "entropy"       .= ent
+                                                                                        ]
+
+-- protocol framing see http://sockjs.github.io/sockjs-protocol/sockjs-protocol-0.3.html
+wsApplication application pending = do
+    connection <- WS.acceptRequest pending
+    WS.sendTextData connection (T.pack "o") -- sockjs client expects an "o" message to open socket
+    _ <- forkIO $ heartbeat connection
+    application connection
+
+heartbeat connection = do
+    WS.sendTextData connection (T.pack "h")
+    threadDelay 25000 -- default in sockjs js implementation
+    heartbeat connection
+
+-- utils
+headerJSON :: H.ResponseHeaders
+headerJSON = [("Content-Type", "application/json; charset=UTF-8")]
+
+headerNotCached :: H.ResponseHeaders
+headerNotCached = [("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0")]
+
+headerCORS :: B.ByteString -> Wai.Request -> H.ResponseHeaders
+headerCORS def req = allowHeaders ++ allowOrigin ++ allowCredentials
+    where allowCredentials = [("Access-Control-Allow-Credentials", "true")]
+          allowHeaders =
+              case lookup "Access-Control-Request-Headers" $ Wai.requestHeaders req of
+                   Just "" -> []
+                   Just ah -> [("Access-Control-Allow-Headers", ah)]
+                   Nothing -> []
+          allowOrigin =
+              case origin of
+                   ""     -> []
+                   "null" -> [("Access-Control-Allow-Origin", def)]
+                   _      -> [("Access-Control-Allow-Origin", origin)]
+          origin = fromMaybe def . lookup "Origin" $ Wai.requestHeaders req
+
+-- remove square brackets and quotes ["msg"]
+receiveData connection = WS.receiveData connection :: IO T.Text
+-- add square brackets and quotes
+sendTextData connection msg =
+    WS.sendTextData connection $ T.concat ["a", msg] -- msg already in an encoded array at reception
--- src/Main.hs
+++ src/Main.hs
@@ -1,76 +1,10 @@
-{-# LANGUAGE OverloadedStrings #-}
 import           Control.Monad      (forever)
-import           Control.Monad.Trans (liftIO)
-import           Control.Concurrent (forkIO, threadDelay)
-import           System.Random (randomRIO)
-import           Data.Maybe (fromMaybe)
-
-import qualified Network.WebSockets as WS
-import qualified Network.HTTP.Types          as H
-import qualified Network.HTTP.Types.Header   as H
-import qualified Network.Wai as Wai
-import qualified Network.Wai.Handler.Warp as Warp
-import qualified Network.Wai.Handler.WebSockets as WaiWS
-import Blaze.ByteString.Builder (fromLazyByteString)
-
 import qualified Data.Text          as T
-import qualified Data.ByteString    as B
-
-import Data.Aeson
-
+import Debate.Server
 
 -- info query should return something sensible, and then move on to websocket
-main = do
-    let settings = Warp.setPort 8888 Warp.defaultSettings
-    Warp.runSettings settings $ WaiWS.websocketsOr WS.defaultConnectionOptions application httpApplication
+main = runServer Config {port = 8888} echo
 
--- TODO: add routing for '/info' and else 404
-httpApplication :: Wai.Application
-httpApplication req respond = do ent <- liftIO $ randomRIO ((0, 4294967295) :: (Int, Int))
-                                 respond $ Wai.responseBuilder H.status200 
-                                           (concat [headerJSON, headerNotCached, headerCORS "*" req])
-                                         $ fromLazyByteString $ encode         [ "websocket"     .= True
-                                                                               , "cookie_needed" .= False
-                                                                               , "origins"       .= ["*:*" :: T.Text]
-                                                                               , "entropy"       .= ent
-                                                                               ]
-
-application pending = do
-    connection <- WS.acceptRequest pending
-    WS.sendTextData connection (T.pack "o") -- sockjs client expects an "o" message to open socket
-    _ <- forkIO $ heartbeat connection
-    echo connection
-
-heartbeat connection = do
-    WS.sendTextData connection (T.pack "h")
-    threadDelay 25000 -- default in sockjs js implementation
-    heartbeat connection
-
--- protocol framing
--- http://sockjs.github.io/sockjs-protocol/sockjs-protocol-0.3.html
--- todo implement close message (receive c[code, reason])
 echo connection = forever $ do
-    msg <- WS.receiveData connection :: IO T.Text
-    putStrLn $ show msg
-    WS.sendTextData connection $ T.concat ["a", msg] -- msg already in an encoded array at reception
-
-headerJSON :: H.ResponseHeaders
-headerJSON = [("Content-Type", "application/json; charset=UTF-8")]
-
-headerNotCached :: H.ResponseHeaders
-headerNotCached = [("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0")]
-
-headerCORS :: B.ByteString -> Wai.Request -> H.ResponseHeaders
-headerCORS def req = allowHeaders ++ allowOrigin ++ allowCredentials
-    where allowCredentials = [("Access-Control-Allow-Credentials", "true")]
-          allowHeaders =
-              case lookup "Access-Control-Request-Headers" $ Wai.requestHeaders req of
-                   Just "" -> []
-                   Just ah -> [("Access-Control-Allow-Headers", ah)]
-                   Nothing -> []
-          allowOrigin =
-              case origin of
-                   ""     -> []
-                   "null" -> [("Access-Control-Allow-Origin", def)]
-                   _      -> [("Access-Control-Allow-Origin", origin)]
-          origin = fromMaybe def . lookup "Origin" $ Wai.requestHeaders req
+    msg <- receiveData connection :: IO T.Text
+    sendTextData connection msg
