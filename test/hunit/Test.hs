1e701a59eea8b5f53cde94b05e99c2b980019861
Elise Huard <elise@jabberwocky.eu>
2014-08-28 14:56:44 +0200

    first tests for xhr polling

--- test/hunit/Test.hs
+++ test/hunit/Test.hs
@@ -1,49 +1,41 @@
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import Test.HUnit hiding (test)
 import Test.Framework.Providers.HUnit
 import Test.Framework.TH
 import Test.Framework
---import Network.Wai.Test
 
---import Network.Wai
+import Debate.Server
 
-
---import qualified Data.Map as Map
+import Network.Wai
+import Network.Wai.Test
+import qualified Data.Text as T
+import Control.Monad (forever)
 
 main =
   defaultMain [
-    --testGroup "xhr-polling" [ testCase "open connection" testXHRtest ]
+    testGroup "basic sockjs" [ testCase "info request" caseInfoRequest ],
+    testGroup "xhr-polling" [ testCase "open connection" caseXHRtest ]
   ]
 
-{-
+echoApp connection = forever $ do
+    msg <- receiveData connection :: IO T.Text
+    sendTextData connection msg
 
-testXHRtest = do let url = "/test/test/xhr"
-                 flip runSession jsonpApp $ do
-                     sres1 <- request defaultRequest
-                                 { queryString = [("callback", Just "test")]
-                                 , requestHeaders = [("Accept", "text/javascript")]
-                                 }
-                     assertContentType "text/javascript" sres1
-                     assertBody "test({\"foo\":\"bar\"})" sres1
-                 
+caseInfoRequest = flip runSession (httpApplication Config {port = 8881, prefix = "/foo"} echoApp) $ do
+    infoResponse <- request defaultRequest
+                { requestMethod = "GET"
+                , rawPathInfo = "/foo/info"
+                }
+    assertStatus 200 infoResponse
+    assertHeader "Content-type" "application/json; charset=UTF-8" infoResponse
 
-
-testdata = "A unicode string\x2122"
-url = "/clipboard/..."
-
-ajaxRequest :: StdMethod -> B.ByteString -> Map.Map Text Text
-               -> 
-ajaxRequest method url datacontent = do
-    let params = mapM_ (uncurry byName) $ Map.toList datacontent
-    doRequestHeaders (renderStdMethod method)
-                     url
-                     [("X-Requested-With", "XMLHttpRequest")]
-                     params
-
-testXHRtest = do
-      ajaxRequest GET url Map.empty
-      bodyEquals $ map (chr . fromIntegral)
-                       (B.unpack $ encodeUtf8 testdata)
-      statusIs 200
--}
+caseXHRtest = flip runSession (httpApplication Config {port = 8881, prefix = "/foo"} echoApp) $ do
+    openResponse <- request defaultRequest
+                { requestMethod = "POST"
+                , rawPathInfo = "/foo/000/session/xhr"
+                }
+    assertStatus 200 openResponse
+    assertBody "o\n" openResponse
