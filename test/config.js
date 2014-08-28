7775b6d41272274f8d345553b3adcb8f52def047
Elise Huard <elise@jabberwocky.eu>
2014-08-28 09:16:41 +0200

    config for running haskell ws server

--- test/config.js
+++ test/config.js
@@ -1,12 +1,12 @@
 var path = require("path");
 var config = {};
 
+/*
 config.websocketServer = 'node'
 config.websocketArgs = [path.resolve(__dirname, '../websocket.js')]
-/*
+*/
 config.websocketServer = path.resolve(__dirname, '..', 'dist/build/debate/debate');
 config.websocketArgs = [];
-*/
 console.log(config);
 
 module.exports = config
