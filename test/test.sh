5766b5cff1a8897c4e6946e1ee89cc90e9930853
Attila Gazso <agazso@gmail.com>
2014-09-05 12:50:48 +0200

    Integration test now waits for selenium to be running

--- test/test.sh
+++ test/test.sh
@@ -3,8 +3,16 @@
 start-selenium &
 selenium_pid=$!
 
-# TODO make sure that selenium is up and running
-sleep 1
+# make sure that selenium is up and running
+selenium_running=0
+while [ $selenium_running -eq 0 ]; do
+	echo "Sleeping..."
+	sleep 1
+	curl 'http://localhost:4444/wd/hub'
+	if [ $? -eq 0 ]; then
+		selenium_running=1
+	fi
+done
 
 node test/test.js
 status=$?
