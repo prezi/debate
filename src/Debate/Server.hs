3fad1ba3933b5ab244ec7ff115fcfa4598687791
Elise Huard <elise@jabberwocky.eu>
2014-09-05 15:16:57 +0200

    some cleanup

--- src/Debate/Server.hs
+++ src/Debate/Server.hs
@@ -9,73 +9,34 @@ module Debate.Server
 )
 where
 
-import           Control.Monad (liftM, forever)
+import           Control.Monad (forever)
 import           Control.Monad.Trans (liftIO)
 import           Control.Concurrent (forkIO, threadDelay)
 import           Control.Concurrent.STM
-import           Control.Concurrent.STM.TVar
-import           Control.Concurrent.STM.TChan
-import           Control.Concurrent.STM.TMVar
 import           Control.Concurrent.Async
-import           GHC.Conc.Sync (atomically)
 import           System.Random (randomRIO)
 import           Data.Maybe (fromMaybe, fromJust)
 
 import qualified Network.WebSockets as WS
 import qualified Network.WebSockets.Connection as WS
 import qualified Network.HTTP.Types          as H
-import qualified Network.HTTP.Types.Header   as H
 import qualified Network.Wai as Wai
 import qualified Network.Wai.Handler.Warp as Warp
 import qualified Network.Wai.Handler.WebSockets as WaiWS
-import Blaze.ByteString.Builder (fromLazyByteString)
-import Data.Text.Encoding (encodeUtf8, decodeUtf8)
+import           Blaze.ByteString.Builder (fromLazyByteString)
+import           Data.Text.Encoding (encodeUtf8, decodeUtf8)
 
 import qualified Data.Text          as T
 import qualified Data.ByteString    as B
 import qualified Data.ByteString.Lazy as L (fromChunks)
-import Data.Aeson
+import           Data.Aeson
 import qualified Data.Map.Strict as Map
 
-import System.Timeout
+import           System.Timeout
 
-data Config = Config { port :: Int,
-                       prefix :: T.Text }
+import           Debate.Types
 
-type SessionId = T.Text
-
-data Client = Client
-  { sessionID :: SessionId
-  , receiveChan :: TChan T.Text
-  , pendingMessages :: TMVar [T.Text]
-  }
-
-data ServerState = ServerState {
-    clients :: TVar (Map.Map SessionId Client)
-  }
-
-data Frame = ControlFrame ControlFrame
-           | DataFrame [T.Text]
-
-data ControlFrame = OpenFrame
-                  | HeartbeatFrame
-                  | CloseFrame
-
-data SockConnection = SockConnection {
-                      receiveData :: IO T.Text
-                    , sendTextData :: T.Text -> IO ()
-                    }
-
-frameToText :: Frame -> T.Text
-frameToText (ControlFrame OpenFrame) =  "o\n"
-frameToText (DataFrame msgs) = T.concat ["a[\"", T.intercalate "\",\""  msgs, "\"]\n"]
-frameToText (ControlFrame HeartbeatFrame) = "h\n"
-frameToText (ControlFrame CloseFrame) = "c\n"
-
-newServerState :: IO ServerState
-newServerState = do
-    newVar <- newTVarIO Map.empty
-    return ServerState {clients = newVar}
+import Debug.Trace
 
 -- TODO close and cleanup
 -- TODO error handling
@@ -93,18 +54,31 @@ runServer configuration application = do
 -- add '/xhr' for xhr-polling
 -- todo routing on prefix too (threading)
 httpApplication configuration application state req respond = do
-                                 let pathPrefix = prefix configuration
-                                 case Wai.pathInfo req of
-                                   [pathPrefix, "info"] -> responseInfo req respond
-                                   [pathPrefix, serverId, sessionId, "xhr_send"] -> processXHR sessionId state req respond
-                                   [pathPrefix, serverId, sessionId, "xhr"] -> pollXHR application sessionId state req respond
-                                   path -> do print path
-                                              respond $ Wai.responseLBS H.status404 [("Content-Type", "text/plain")] "Not found"
+                                 let (pathPrefix, _) = H.decodePath $ encodeUtf8 $ prefix configuration -- can be more than one /
+                                     path = Wai.pathInfo req
+                                 if matchPrefix pathPrefix path
+                                   then routing path application state req respond
+                                   else response404 respond
+
+matchPrefix :: [T.Text] -> [T.Text] -> Bool
+matchPrefix [] rest = True
+matchPrefix (x:xs) (y:ys) = (x == y) && matchPrefix xs ys
+matchPrefix (x:_) [] = False
+
+routing pathInfo application state req respond = 
+                    case pathInfo of
+                        [_, "info"] -> responseInfo req respond
+                        [_, _, sessionId, "xhr_send"] -> processXHR sessionId state req respond
+                        [_, _, sessionId, "xhr"] -> pollXHR application sessionId state req respond
+                        path -> do print path
+                                   response404 respond
+
+response404 respond = respond $ Wai.responseLBS H.status404 [("Content-Type", "text/plain")] "Not found"
 
 responseInfo req respond = do
                 ent <- liftIO $ randomRIO ((0, 4294967295) :: (Int, Int))
                 respond $ Wai.responseBuilder H.status200 
-                    (concat [headerJSON, headerNotCached, headerCORS "*" req])
+                    (commonHeaders req)
                     $ fromLazyByteString $ encode [ "websocket"     .= True
                                                    , "cookie_needed" .= False
                                                    , "origins"       .= ["*:*" :: T.Text]
@@ -122,17 +96,15 @@ openXHR application state@ServerState{..} sessionId req respond = do
                    addClient state sessionId receiveChan
                    _ <- forkIO $ runApplication application sessionId state receiveChan
                    respond $ Wai.responseLBS H.status200
-                      (concat [headerJSON, headerNotCached, headerCORS "*" req]) $ L.fromChunks [encodeUtf8 $ frameToText (ControlFrame OpenFrame)]
+                      (commonHeaders req) $ L.fromChunks [encodeUtf8 $ frameToText (ControlFrame OpenFrame)]
 
-addClient state@ServerState{..} sessionId receiveChan = atomically $ do
+addClient :: ServerState -> SessionId -> TChan T.Text -> IO ()
+addClient ServerState{..} sessionId receiveChan = atomically $ do
                    clientMap <- readTVar clients
                    emptyPending <- newEmptyTMVar
                    writeTVar clients $ Map.insert sessionId Client { sessionID = sessionId, pendingMessages = emptyPending, receiveChan = receiveChan } clientMap
 
-pendingMessagesXHR sessionId state@ServerState{..} req respond = do
-                   clientMap <- readTVarIO clients
-                   let client = fromJust $ Map.lookup sessionId clientMap
-                   timedResponse 5000000 (getPendingMessages sessionId state) (msgResponse respond req) (emptyResponse respond req)
+pendingMessagesXHR sessionId state@ServerState{..} req respond = timedResponse 5000000 (msgFromApplication sessionId state) (msgResponse respond req) (emptyResponse respond req)
 
 timedResponse delay function endedFunction timeoutFunction = do
                    result <- timeout delay function
@@ -141,43 +113,49 @@ timedResponse delay function endedFunction timeoutFunction = do
                      Just msg -> endedFunction msg
 
 emptyResponse respond req = respond $ Wai.responseLBS H.status200  
-                               (concat [headerJSON, headerNotCached, headerCORS "*" req]) ""
+                               (commonHeaders req) ""
 
 msgResponse respond req msg = respond $ Wai.responseLBS H.status200  
-                                 (concat [headerJSON, headerNotCached, headerCORS "*" req]) $ L.fromChunks [encodeUtf8 $ frameToText (DataFrame msg)]
+                                 (commonHeaders req) $ L.fromChunks [encodeUtf8 $ frameToText (DataFrame msg)]
 
-processXHR sessionId ServerState{..} req respond = do 
+processXHR sessionId state req respond = do 
                    body <- Wai.requestBody req
-                   let msg = msgFromFrame . decodeUtf8 $ body -- todo error handling on empty body
-                   clientMap <- readTVarIO clients
-                   let client = fromJust $ Map.lookup sessionId clientMap
-                   atomically $ writeTChan (receiveChan client) msg -- msg to application
+                   let msg = msgFromFrame . decodeUtf8 $ body
+                   msgToApplication sessionId state msg
                    respond $ Wai.responseLBS H.status204 
-                    (concat [headerJSON, headerNotCached, headerCORS "*" req]) ""
+                    (commonHeaders req) ""
+
+-- application is running on separate thread
+-- * gets sent msgs in TChan
+-- * delivers msg to TMVar
+msgToApplication :: SessionId -> ServerState -> T.Text -> IO ()
+msgToApplication sessionId ServerState{..} msg = atomically $ do
+                   clientMap <- readTVar clients
+                   let client = fromJust $ Map.lookup sessionId clientMap
+                   writeTChan (receiveChan client) msg
+
+msgFromApplication :: SessionId -> ServerState -> IO [T.Text]
+msgFromApplication sessionId ServerState{..} = atomically $ do
+    clientMap <- readTVar clients
+    let client = fromJust $ Map.lookup sessionId clientMap
+    takeTMVar (pendingMessages client)
 
 runApplication application sessionId state receive = forever $
     application SockConnection { receiveData = atomically $ readTChan receive, sendTextData = sendMessage }
     where sendMessage = addPendingMessages sessionId state
 
 -- add msg to TVar as a queue
+addPendingMessages :: SessionId -> ServerState -> T.Text -> IO ()
 addPendingMessages sessionId ServerState{..} msg = atomically $ do
     clientMap <- readTVar clients
     let client = fromJust $ Map.lookup sessionId clientMap
     putTMVar (pendingMessages client) [msg]
-    writeTVar clients $ Map.update (\c -> Just client) sessionId clientMap
-
--- cleanPendingMessages client = Client { sessionID = sessionID client, receiveChan = receiveChan client, pendingMessages = newTMVar }
-
--- get pending messages and empty out the TVar
-getPendingMessages sessionId state@ServerState{..} = atomically $ do
-    clientMap <- readTVar clients
-    let client = fromJust $ Map.lookup sessionId clientMap
-    takeTMVar (pendingMessages client)
+    writeTVar clients $ Map.update (\_ -> Just client) sessionId clientMap
 
 -- protocol framing see http://sockjs.github.io/sockjs-protocol/sockjs-protocol-0.3.html
-wsApplication application state pending@WS.PendingConnection {pendingRequest = WS.RequestHead path headers _} = do
+wsApplication application state pending@WS.PendingConnection {pendingRequest = WS.RequestHead path _ _} = do
     -- path expected: prefix - server Id - session Id - 'websocket'
-    let (pathInfo, query) = H.decodePath path
+    let (pathInfo, _) = H.decodePath path
     print pathInfo
 
     receive <- atomically newTChan
@@ -187,7 +165,7 @@ wsApplication application state pending@WS.PendingConnection {pendingRequest = W
     WS.sendTextData connection (T.pack "o") -- sockjs client expects an "o" message to open socket
     _ <- forkIO $ heartbeat connection
     _ <- forkIO $ runApplication application "randomsessionId" state receive
-    race (receiveLoop receive connection) (sendLoop state connection)
+    _ <- race (receiveLoop receive connection) (sendLoop state connection)
     return ()
 
 receiveLoop receive connection =
@@ -196,7 +174,7 @@ receiveLoop receive connection =
        atomically $ writeTChan receive $ msgFromFrame msg
 sendLoop state connection =
     forever $ do
-       msg <- getPendingMessages "randomsessionId" state
+       msg <- msgFromApplication "randomsessionId" state
        WS.sendTextData connection $ msgToFrame msg
 
 -- TODO XHR heartbeat. from sockjs-protocol:
@@ -208,6 +186,7 @@ heartbeat connection = do
     heartbeat connection
 
 -- utils
+commonHeaders :: Wai.Request -> [H.Header]
 commonHeaders req = concat [headerJSON, headerNotCached, headerCORS "*" req]
 
 headerJSON :: H.ResponseHeaders
@@ -231,6 +210,8 @@ headerCORS def req = allowHeaders ++ allowOrigin ++ allowCredentials
                    _      -> [("Access-Control-Allow-Origin", origin)]
           origin = fromMaybe def . lookup "Origin" $ Wai.requestHeaders req
 
+msgFromFrame :: T.Text -> T.Text
 msgFromFrame msg = T.splitOn "\"" msg !! 1
 
+msgToFrame :: [T.Text] -> T.Text
 msgToFrame msgs = frameToText $ DataFrame msgs
---
+++ src/Debate/Types.hs
@@ -0,0 +1,57 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Debate.Types (
+  Config(..)
+, SessionId
+, Client(..)
+, ServerState(..)
+, Frame(..)
+, ControlFrame(..)
+, SockConnection(..)
+, frameToText
+, newServerState
+)
+where
+
+import           Control.Concurrent.STM.TMVar
+import           Control.Concurrent.STM.TVar
+import           Control.Concurrent.STM.TChan
+import qualified Data.Map.Strict as Map
+import qualified Data.Text          as T
+
+data Config = Config { port :: Int
+                     , prefix :: T.Text }
+
+type SessionId = T.Text
+
+data Client = Client
+  { sessionID :: SessionId
+  , receiveChan :: TChan T.Text
+  , pendingMessages :: TMVar [T.Text]
+  }
+
+data ServerState = ServerState {
+    clients :: TVar (Map.Map SessionId Client)
+  }
+
+data Frame = ControlFrame ControlFrame
+           | DataFrame [T.Text]
+
+data ControlFrame = OpenFrame
+                  | HeartbeatFrame
+                  | CloseFrame
+
+data SockConnection = SockConnection {
+                      receiveData :: IO T.Text
+                    , sendTextData :: T.Text -> IO ()
+                    }
+
+frameToText :: Frame -> T.Text
+frameToText (ControlFrame OpenFrame) =  "o\n"
+frameToText (DataFrame msgs) = T.concat ["a[\"", T.intercalate "\",\""  msgs, "\"]\n"]
+frameToText (ControlFrame HeartbeatFrame) = "h\n"
+frameToText (ControlFrame CloseFrame) = "c\n"
+
+newServerState :: IO ServerState
+newServerState = do
+    newVar <- newTVarIO Map.empty
+    return ServerState {clients = newVar}
