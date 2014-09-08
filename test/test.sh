c555271a6763918837200b45e66dc4c66234fa98
Attila Gazso <agazso@gmail.com>
2014-09-08 18:56:02 +0200

    More informative integration test script output

--- test/test.sh
+++ test/test.sh
@@ -3,10 +3,9 @@
 start-selenium &
 selenium_pid=$!
 
-# make sure that selenium is up and running
 selenium_running=0
 while [ $selenium_running -eq 0 ]; do
-	echo "Sleeping..."
+	echo "Waiting for Selenium to start..."
 	sleep 1
 	curl 'http://localhost:4444/wd/hub'
 	if [ $? -eq 0 ]; then
@@ -14,10 +13,12 @@ while [ $selenium_running -eq 0 ]; do
 	fi
 done
 
+echo "\n\nTesting Websocket...\n"
 node test/test.js
 status=$?
 
 if [ $status -eq 0 ]; then
+  echo "\n\nTesting XHR-polling...\n"
   node test/test_xhr.js
   status=$?
 fi
