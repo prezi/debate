00dcbeb8bdd1aa72bcb015787f33fe9fee135a6a
Elise Huard <elise@jabberwocky.eu>
2014-10-21 16:52:53 +0200

    polling status client

--- public/chat/js/status-client.js
+++ public/chat/js/status-client.js
@@ -5,15 +5,21 @@ $( document ).ready(function() {
     var reconnect = function() {
         var sockjs = new SockJS(sockjs_url, undefined, {protocols_whitelist: ['websocket', 'xhr-polling'], debug: true});
 
+        var pollStatus = function() {
+            console.log("polling ...");
+            sockjs.send(JSON.stringify({message: "STATUS user1 pass1"}));
+        };
+
         // when connecting, should indicated to user that connected, and that should login
         sockjs.onopen    = function()  {
-            sockjs.send(JSON.stringify({message: "STATUS user1 pass1"}));
+            pollStatus();
         };
         // handle refuse access to room/join room, leave room
         sockjs.onmessage = function(e) {
           var message = $.parseJSON(e.data);
           console.log(message);
           $("#status").text(e.data);
+          setTimeout(pollStatus, 10000); // poll every x
         };
         sockjs.onclose = function(e) {
                 // broadcast leaving in common room: let server know?
