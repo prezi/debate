4599d095d906cddf8bdddb259154923d61e0e633
Elise Huard <elise@jabberwocky.eu>
2014-10-23 11:51:38 +0200

    refactored ws-test a little

--- test/ws-test.js
+++ test/ws-test.js
@@ -1,5 +1,53 @@
 var WebSocket = require('ws');
 
+// actual test
+var testFunc = function(ws) {
+
+    message(ws, null, "LOGIN test1 pass1", null);
+
+    message(ws, "test1", "MSG hello there", "Lobby");
+
+    message(ws, "test1", "LOGOUT", "Lobby");
+}
+
+var wsTest = function(testFunction) {
+    // random server id
+    var server = random_number_string(1000);
+    // session id
+    var connid = random_string(8);
+
+    // making the sockjs url
+    var ws = new WebSocket('ws://localhost:8989/chat' + '/' + server + '/' + connid + '/' + 'websocket');
+
+    ws.on('message', function(data, flags) {
+        var type = data.slice(0, 1);
+        switch(type) {
+        case 'o':
+            console.log('open');
+
+            testFunction(ws);
+
+            setTimeout(function() { ws.close();}, 1000);
+
+            break;
+        case 'a':
+            var payload = JSON.parse(data.slice(1) || '[]');
+            console.log(payload);
+            break;
+        case 'h':
+            //console.log('heartbeat');
+            break;
+        }
+    });
+};
+
+var message = function(ws, user, payload, channel) {
+   var msg = {user: user, message: payload, channel: channel};
+
+   // double stringification: one from client, one from sockjs lib
+   ws.send(JSON.stringify([JSON.stringify(msg)]));
+};
+
 // random string function from sockjs
 var random_string_chars = 'abcdefghijklmnopqrstuvwxyz0123456789_';
 var random_string = function(length, max) {
@@ -20,48 +68,5 @@ var random_number_string = function(max) {
     var p = Array(t+1).join('0');
     return (p + random_number(max)).slice(-t);
 };
-// random server id
-var server = random_number_string(1000);
-// session id
-var connid = random_string(8);
 
-// making the sockjs url
-var ws = new WebSocket('ws://localhost:8989/chat' + '/' + server + '/' + connid + '/' + 'websocket');
-
-ws.on('open', function() {
-
-});
-ws.on('message', function(data, flags) {
-    var type = data.slice(0, 1);
-    switch(type) {
-    case 'o':
-        console.log('open');
-
-        message(null, "LOGIN test1 pass1", null);
-
-        message("test1", "MSG hello there", "Lobby");
-
-        message("test1", "LOGOUT", "Lobby");
-
-        setTimeout(function() {ws.close();}, 1000);
-
-        break;
-    case 'a':
-        var payload = JSON.parse(data.slice(1) || '[]');
-        console.log('message', payload);
-        break;
-    case 'c':
-        console.log('close');
-        break;
-    case 'h':
-        //console.log('heartbeat');
-        break;
-    }
-});
-
-var message = function(user, payload, channel) {
-   var msg = {user: user, message: payload, channel: channel};
-
-   // double stringification: one from client, one from sockjs lib
-   ws.send(JSON.stringify([JSON.stringify(msg)]));
-};
+wsTest(testFunc);
